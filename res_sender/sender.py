33381f38cc21e4631f64c28dd90e3b821b6b5cab
giga89 <andrea.ravalli@gmail.com>
2023-11-26 00:16:36 +0100

    Removed biomassa Calculation of quantity per user

--- res_sender/sender.py
+++ res_sender/sender.py
@@ -33,17 +33,16 @@ def add_point_to_user_in_array(user, point_to_add):
 if __name__ == "__main__":
 
     #resources
-    res_arr_str = ["arco", "biomass","carbon","copperOre","diamond","ironOre","lumanite","rochinol"] 
-    res_arr_val = [446, 0, 100, 142, 445, 136, 193, 450] #random numbers waiting fonta/tori
+    res_arr_str = ["arco","carbon","copperOre","diamond","ironOre","lumanite","rochinol"] 
+    res_arr_val = [446, 100, 142, 445, 136, 193, 450] #random numbers waiting fonta/tori
     res_arr_address = ["ARCoQ9dndpg6wE2rRexzfwgJR3NoWWhpcww3xQcQLukg", 
-                    "MASS9GqtJz6ABisAxcUn3FeR4phMqH1XfG6LPKJePog",
                     "CARBWKWvxEuMcq3MqCxYfi7UoFVpL9c4rsQS99tw6i4X",
                     "CUore1tNkiubxSwDEtLc3Ybs1xfWLs8uGjyydUYZ25xc",
                     "DMNDKqygEN3WXKVrAD4ofkYBc4CKNRhFUbXP4VK7a944",
                     "FeorejFjRRAfusN9Fg3WjEZ1dRCf74o6xwT5vDt3R34J",
                     "LUMACqD5LaKjs1AeuJYToybasTXoYQ7YkxJEc4jowNj",
                     "RCH1Zhg4zcSSQK8rw2s6rDMVsgBEWa4kiv1oLFndrN5"] 
-    res_array_total = [0, 0, 0, 0, 0, 0, 0, 0]
+    res_array_total = [0, 0, 0, 0, 0, 0, 0]
 
     #privkey of sender
     privKey_arg = sys.argv[1]
@@ -82,3 +81,12 @@ if __name__ == "__main__":
         print(res_arr_str[index] + " " +str(res))
         index=index+1
 
+    #populate array of user from files
+    index=0
+    for resource in res_arr_str:
+        for user in users_array:
+            quantity_to_divide = res_array_total[index]
+            if quantity_to_divide>0:
+                quantity_for_this_resource = (user.points / 6) / res_arr_val[index]
+                print(user.name + " have to " + str(quantity_for_this_resource) + " of " + res_arr_str[index])
+        index=index+1
